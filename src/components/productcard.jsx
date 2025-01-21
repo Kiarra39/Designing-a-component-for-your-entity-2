1c97beb2e194d73115c1593b944fdcf2e90ec87f
Kiesha Maria <kiesha.maria@kalvium.community>
2025-01-21 10:26:02 +0530

    assignment completed

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import React from 'react';
+import ProductButton from './button.jsx'; 
+
+const Productcard = () => {
+    const image = 'https://cdn.thewirecutter.com/wp-content/media/2023/07/bluetoothheadphones-2048px-0876.jpg'; 
+    const name = 'Headphones';
+    const price = '2,500';
+  
+    return (
+      <div className="product-card">
+        <img src={image} alt={name} className="product-image"  style={{ width: '100%', height: '200px', objectFit: 'cover', borderRadius: '8px' }}  />
+        <h2 className="product-name">{name}</h2>
+        <p className="product-price">${price}</p>
+        <ProductButton />
+      </div>
+    );
+  };
+
+export default Productcard;
